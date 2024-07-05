5d461381d91c175dc71e91a8a4c5c08c9cc7df15
randi089 <randifebriadi@gmail.com>
2024-07-05 11:00:56 +0700

    change icon

--- js/script.js
+++ js/script.js
@@ -1,6 +1,32 @@
+// Menu mobile
 const menuToggle = document.querySelector(".menu-toggle");
 const nav = document.querySelector("nav ul");
 
 menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
 });
+
+// Smooth Scrolling
+document.addEventListener("DOMContentLoaded", function () {
+  // ambil objeck href
+  const target = document.querySelector('a[href^="#"]');
+
+  target.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const t = target.getAttribute("href").substring(1);
+    smoothScroll(t);
+  });
+});
+
+// function Smooth Scrolling
+function smoothScroll(t) {
+  const element = document.getElementById(t);
+
+  if (element) {
+    window.scrollTo({
+      top: element.offsetTop,
+      behavior: "smooth",
+    });
+  }
+}
